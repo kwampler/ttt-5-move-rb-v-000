e3a003aa321e30cd6ecbc013b4a2c27fa0f96dfe
katejsenger <katejsenger@yahoo.com>
2018-09-28 21:26:51 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,8 +6,8 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(input)
-  input.to_index(input).to_i 
-  input_to_index(input) - 1
+def input_to_index(user_input)
+  input.to_index(user_input).to_i 
+  input_to_index(user_input) - 1
   puts "1", "2", "3", "4", "5", "6", "7", "8", "9"
 end
