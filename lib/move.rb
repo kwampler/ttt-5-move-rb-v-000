328759f114d8e00aed129ff8115b4dc802afb201
katejsenger <katejsenger@yahoo.com>
2018-09-28 21:06:44 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,6 +11,9 @@ def input_to_index(input)
 end
 
 def move
+  def update_array_at_with(array,index, value)
+    array[index] = value
+  end
   gets.display_board(board)
   gets.input_to_index(input)
   gets.
